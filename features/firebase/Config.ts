54c842ca022909b1bb64ffedbc3a34262a729285
Lamar Redd <145939771+reddlamar@users.noreply.github.com>
2024-07-18 16:53:10 -0700

    add environment variables

--- features/firebase/Config.ts
+++ features/firebase/Config.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArIxY7Nc_YTZeWa-B_A3JXtnfDg7Q0yzE",
-  authDomain: "techcourses-67ec1.firebaseapp.com",
-  projectId: "techcourses-67ec1",
-  storageBucket: "techcourses-67ec1.appspot.com",
-  messagingSenderId: "1084752641368",
-  appId: "1:1084752641368:web:7227cb4263eb0fcbc4b92d",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
