b5deaf19e05ff62aef784f6c6c9c0907decfe676
Lamar Redd <145939771+reddlamar@users.noreply.github.com>
2024-07-24 09:10:14 -0700

    implementing firebase auth

--- features/firebase/Config.ts
+++ features/firebase/Config.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -14,6 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
 
 export const firebaseDB = getFirestore(app);
 
